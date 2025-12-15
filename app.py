696e622e74b31e033bdaf8cd8fea1241d8f19d34
Heidi Mara do Rosário Sousa <sousaheidimara@gmail.com>
2025-12-15 10:57:54 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import time
 from streamlit_option_menu import option_menu
 
 # Configuracao inicial da pagina
 st.header("Introduzindo os Elementos de Streamlit")
+menu = option_menu (menu_title="Menu",
+                    options=["Início", "Gráficos Estatísticos", "Gráficos Dinâmicos", "Widgets", "Formulario"],
+                    icons =["house, "bar-chart", "bar-chart-line", "toggles", "bar-chart"],
+                    menu_icon="cast",
+                    default_index=0,
+                    orientation="horizontal"
+                    )
+                    
