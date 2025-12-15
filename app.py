f0e5ba751b9dc9c58565ba6261a13ac84d5af587
Heidi Mara do Rosário Sousa <sousaheidimara@gmail.com>
2025-12-15 10:49:40 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+import time
 from streamlit_option_menu import option_menu
 
 # Configuracao inicial da pagina
-st.set_page_config(page_title="Dashboard Empresarial", layout="wide")
-
-# Menu lateral com Streamlit Option Menu
-menu = option_menu("Menu", ["Inicio", "Visualizar Dados", "Previsao", "Sobre"],
-                       icons=["house", "bar-chart", "graph-up", "info-circle"],
-                       menu_icon="cast", default_index=0)
+st.header("Introduzindo os Elementos de Streamlit")
