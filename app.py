5ae3890742fc2f8016d18b688cee4e51de117fd7
Heidi Mara do Rosário Sousa <sousaheidimara@gmail.com>
2025-12-15 11:02:16 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import time
+#import time
 from streamlit_option_menu import option_menu
 
 # Configuracao inicial da pagina
